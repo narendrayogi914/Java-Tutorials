ede85748a4546b99d13f0226f9da851296d4ab30
narendrayogi914 <narendrayogi914@gmail.com>
2024-03-21 16:07:10 +0530

    add array file in repo

---
+++ Array.java
@@ -0,0 +1,22 @@
+public class Array {
+     public static void main(String[] args) {
+        // int num[] ={4,5,3,2};
+        
+        // // System.out.println(num[2]);
+        // // System.out.println(num[0]);
+        // // System.out.println(num[1]);
+        // for(int i =0 ; i<4;i++){
+        //     System.out.print(num[i]+" ");
+
+        // }
+
+        int num[]= new int[4];
+        String names[] ={"narendra","mayank","garvit","sachin"};
+        for(int i=0;i<4;i++){
+            System.out.print(names[i]+ " ");
+        }
+
+
+     }
+    
+}
